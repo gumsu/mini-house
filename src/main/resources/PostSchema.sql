48db705160613b1c28bbf664ee55b21a60754d8f
gumsu <chavi9@naver.com>
2022-09-18 23:08:04 +0900

    members create sql 추가

--- src/main/resources/PostSchema.sql
+++ src/main/resources/PostSchema.sql
@@ -4,5 +4,8 @@ CREATE TABLE POSTS(
                       content varchar(1000) not null,
                       writer varchar(10) not null,
                       createdDate datetime not null,
-                      modifiedDate datetime null
+                      modifiedDate datetime null,
+                      memberId INT null, -- 임시 null
+                      FOREIGN KEY (memberId)
+                      REFERENCES MEMBERS(id) on update cascade
 )
