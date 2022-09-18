3287c8c8e3299f6553d0a4f229942567f7bd822b
gumsu <chavi9@naver.com>
2022-09-19 01:16:55 +0900

    member 회원가입 controller test 작성

--- src/main/java/com/example/board/request/MemberSaveRequest.java
+++ src/main/java/com/example/board/request/MemberSaveRequest.java
@@ -2,9 +2,13 @@ package com.example.board.request;
 
 import com.example.board.domain.member.Member;
 import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
+@NoArgsConstructor
+@Getter
 public class MemberSaveRequest {
 
     private String name;
