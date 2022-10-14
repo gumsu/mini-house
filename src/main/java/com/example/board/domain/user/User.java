855c7758cd8fd657150a793665e338e7da2c5f70
gumsu <chavi9@naver.com>
2022-10-14 18:38:49 +0900

    [#12] feat: spring security 관련 클래스 제거

--- src/main/java/com/example/board/domain/user/User.java
+++ src/main/java/com/example/board/domain/user/User.java
@@ -1,24 +1,20 @@
 package com.example.board.domain.user;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-import java.util.stream.Collectors;
+import java.time.LocalDateTime;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import lombok.Builder;
 import lombok.Getter;
-
-import javax.persistence.*;
-import java.time.LocalDateTime;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
 
 @Table(name = "users")
 @Getter
 @RequiredArgsConstructor
 @Entity
-public class User implements UserDetails {
+public class User {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -27,49 +23,15 @@ public class User implements UserDetails {
     private String email;
     private String password;
     private LocalDateTime createdAt;
-
-    @ElementCollection(fetch = FetchType.EAGER)
-    private List<String> roles = new ArrayList<>();
+    private String role;
 
     @Builder
-    public User(Long id, String name, String email, String password, LocalDateTime createdAt) {
+    public User(Long id, String name, String email, String password, LocalDateTime createdAt, String role) {
         this.id = id;
         this.name = name;
         this.email = email;
         this.password = password;
         this.createdAt = createdAt;
-        this.roles.add("ROLE_USER");
-    }
-
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return roles.stream()
-            .map(SimpleGrantedAuthority::new)
-            .collect(Collectors.toList());
-    }
-
-    @Override
-    public String getUsername() {
-        return email;
-    }
-
-    @Override
-    public boolean isAccountNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return true;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return true;
+        this.role = role;
     }
 }
