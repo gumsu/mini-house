a6159f05f4f9da20455ed5889ad1e9b3e7ae27b7
gumsu <chavi9@naver.com>
2022-10-24 22:44:21 +0900

    [#12] test: JWTTokenProvider 테스트 수정 - application.yml 로 secret key 이동으로 인해 TestPropertySource 추가

--- src/test/java/com/example/board/domain/auth/JWTTokenProviderTest.java
+++ src/test/java/com/example/board/domain/auth/JWTTokenProviderTest.java
@@ -1,11 +1,23 @@
 package com.example.board.domain.auth;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import com.example.board.exception.InvalidToken;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.env.Environment;
+import org.springframework.test.context.TestPropertySource;
 
+@SpringBootTest
+@TestPropertySource(properties = {"spring.config.location=classpath:application-dev.yml"})
 class JWTTokenProviderTest {
 
+    @Autowired
+    Environment environment;
+
+    @Autowired
     final JWTTokenProvider jwtTokenProvider = new JWTTokenProvider();
 
     @Test
@@ -24,15 +36,15 @@ class JWTTokenProviderTest {
     @Test
     void 잘못된_토큰은_에러() {
         String wrongToken = "aaaaaaaaaaaaaaaaaaaaaaaaaaaa";
-        boolean validateToken = jwtTokenProvider.validateToken(wrongToken);
-        assertThat(validateToken).isFalse();
+        assertThatThrownBy(() -> jwtTokenProvider.validateToken(wrongToken))
+            .isInstanceOf(InvalidToken.class);
     }
 
     @Test
     void 만료된_토큰은_에러() {
         String token = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1c2VybmFtZSIsInJvbGUiOiJVU0VSIiwiaWF0IjoxNjY1NzMzNjYxLCJleHAiOjE2NjU3NDA4NjF9.ry3HrnthQAin_A_vfAV6SR0QSHpCRRX7SrL8SYYQVwU";
-        boolean validateToken = jwtTokenProvider.validateToken(token);
-        assertThat(validateToken).isFalse();
+        assertThatThrownBy(() -> jwtTokenProvider.validateToken(token))
+            .isInstanceOf(InvalidToken.class);
     }
 
     @Test
